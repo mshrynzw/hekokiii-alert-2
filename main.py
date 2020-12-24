c4f503372cf14f97a4309493153e69a0c53e6d4d
mshrynzw <iq87io25@gmail.com>
2020-12-24 18:49:59 +0900

    [test]

--- main.py
+++ main.py
@@ -9,9 +9,8 @@ from twitch import check_twitch_start
 logging.basicConfig(level=logging.INFO, format='%(levelname)s : %(asctime)s : %(message)s')
 
 
-if __name__ == "__main__":
-    logging.debug("start main")
-    if os.environ["IS_TWITCH"]:
-        logging.debug("enter true")
-        thread_twitch = threading.Thread(target=check_twitch_start)
-        thread_twitch.start()
+logging.debug("start main")
+if os.environ["IS_TWITCH"]:
+    logging.debug("enter true")
+    thread_twitch = threading.Thread(target=check_twitch_start)
+    thread_twitch.start()
