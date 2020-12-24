107ad8e289ec2b5c282320cd60eb5c82570af617
mshrynzw <iq87io25@gmail.com>
2020-12-24 16:30:10 +0900

    [test] 20/12/24 16:30

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ import os
 import threading
 from twitch import check_twitch_start
 
+logging.basicConfig(level=logging.INFO, format='%(levelname)s : %(asctime)s : %(message)s')
+
 
 if __name__ == "__main__":
+    logging.debug("start main")
     if os.environ["IS_TWITCH"]:
-        logging.debug("test")
+        logging.debug("enter true")
         thread_twitch = threading.Thread(target=check_twitch_start)
         thread_twitch.start()
