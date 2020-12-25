c816acbae81975ca08e0387e1f4cb4897e18ee61
mshrynzw <iq87io25@gmail.com>
2020-12-25 13:47:14 +0900

    [test]

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import logging
 import os
 import threading
 from twitch import check_twitch_start
+from bbs import check_bbs
 
 
-if os.environ["IS_TWITCH"]:
-    thread_twitch = threading.Thread(target=check_twitch_start)
-    thread_twitch.start()
+if __name__ == "__main__":
+    if os.environ["IS_TWITCH"]:
+        thread_twitch = threading.Thread(target=check_twitch_start)
+        thread_twitch.start()
+
+    if os.environ["IS_5CH"]:
+        thread_5ch = threading.Thread(target=check_bbs)
+        thread_5ch.start()
