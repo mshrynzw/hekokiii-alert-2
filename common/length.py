0bb2ddc052f006de5e4f50c452e5c22caf99f1e5
mshrynzw <iq87io25@gmail.com>
2020-12-27 18:31:04 +0900

    [fix]

--- common/length.py
+++ common/length.py
@@ -17,13 +17,14 @@ def get_east_asian_width_count(text):
 
 def delete_east_asian_width_count(text, delete_count):
     count = 0
+    
     for c in text:
         if unicodedata.east_asian_width(c) in 'FWA':
-            text = text[:-2]
             count += 2
         else:
-            text = text[:-1]
             count += 1
+
+        text = text[:-1]
         if count > delete_count:
             break
 
